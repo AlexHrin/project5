f628486b6f630f6487e34c22ca7e050ded099ce5
Alex Hrin <hrinsasha@hotmail.com>
2022-04-06 09:24:50 +0300

    15

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,33 @@ import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
 
-/*
 import Swiper, { Navigation, Pagination } from 'swiper';
-const swiper = new Swiper();
-*/
+Swiper.use([Navigation, Pagination]);
+// import Swiper and modules styles
+
+// init Swiper:
+
+const swiper = new Swiper('.swiper', {
+   // Optional parameters
+
+   loop: true,
+   slidesPerView: 8.5,
+   spaceBetween: 20,
+   centeredSlides: true,
+
+   // If we need pagination
+   pagination: {
+      el: '.swiper-pagination',
+   },
+
+   // Navigation arrows
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
+
+   // And if we need scrollbar
+   scrollbar: {
+      el: '.swiper-scrollbar',
+   },
+});
