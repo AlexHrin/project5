60aae1d86690bdff33676b1af875dde82133bef7
DESKTOP-EHV52KH\Intel <hrinsasha@hotmail.com>
2022-07-13 14:23:29 +0300

    24

--- src/js/app.js
+++ src/js/app.js
@@ -54,6 +54,17 @@ $(document).ready(function () {
    });
 });
 
+// MENU BURGER
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+   iconMenu.addEventListener("click", function (e) {
+      document.body.classList.toggle('_lock');
+      iconMenu.classList.toggle('_active');
+      menuBody.classList.toggle('_active');
+   });
+}
+
 //// CLOSE COVID INFO
 //var block = document.querySelector('.header__infobox');
 //var button = document.querySelector('.header__close');
